df2990949ca129eddef88ca1f9361d14ab5ec344
Austin T <austin.taranto@gmail.com>
2020-02-19 15:04:55 -0500

    xwalk, ywalk, textshadow

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 const hero = document.querySelector(".hero");
 const text = hero.querySelector("h1");
+// const walk = 100; // 100px
+const walk = 500;
 
 function shadow(e) {
     // console.log(e);
@@ -15,7 +17,19 @@ function shadow(e) {
         y = y + e.target.offsetTop;
     }
 
-    console.log(x, y);
+    // console.log(x, y);
+    const xWalk = Math.round((x / width * walk) - (walk / 2));
+    const yWalk = Math.round((y / height * walk) - (walk / 2));
+
+    // console.log(xWalk, yWalk);
+
+    text.style.textShadow = `
+    ${xWalk}px ${yWalk}px 0 blue,
+    ${xWalk * -1}px ${yWalk * -1}px 0 pink,
+    ${yWalk}px ${xWalk * -1}px 0 yellow,
+    ${yWalk * -1}px ${xWalk}px 0 purple
+    `;
+
 
 }
 
